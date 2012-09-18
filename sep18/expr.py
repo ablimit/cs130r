2f89609580bef2e5f41f36e0a223e59b84ab4cc0
Ablimit Aji <ablimit@gmail.com>
2012-09-18 17:54:01 -0300

    Update sep18/expr.py

--- sep18/expr.py
+++ sep18/expr.py
@@ -1,12 +1,14 @@
-
 #  Expressions 
 # precedence comp < +,- < *,/,//,% < +x,-x < ** < (exp)
 
 weight = 135.5
 height = 70.0
 
+# this is wrong 
 bmi = weight *703 / height * height 
 
+# this is right 
+bmi = weight *703 / (height * height) 
 
 expr = 5*2**3
 print(expr)
