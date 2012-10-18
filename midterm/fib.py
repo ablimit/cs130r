7954af7d0c92a517eb1cac0f129a72cf8353b926
Ablimit Aji <aaji@mathcs.emory.edu>
2012-10-18 16:31:17 -0400

    midterm related problem uploaded

---
+++ midterm/fib.py
@@ -0,0 +1,25 @@
+
+n = int(input("Please input a number:\n"))
+
+a = 0  # first fibonacci nnumber
+b = 1  # second fibonacci nnumber
+c = 0  # temporary variable 
+
+i =1   # counter 
+
+fib_list = [a,b]
+while i < n:
+    c = a + b  # new fibonacci number 
+    a = b      # advance a to next fibonacci number
+    b = c      # advance b to the next fibonacci number
+    fib_list.append(c)
+    i = i+1    # update counter
+
+# print(fib_list)
+
+for i in range(n,0,-1):
+    print(fib_list[i])
+
+
+# print("The fibonacci number is " + str(b))
+
